a121ff7cd72ee7816815083dd05cb91718da316b
Youssef <sahli.youssef@ymail.com>
2022-11-06 19:48:36 +0100

    ship weapons basic functions

--- Ship/Ship.gd
+++ Ship/Ship.gd
@@ -5,10 +5,12 @@ export (float, 0.0, 0.2) var acceleration = 0.1
 export (Array, PackedScene) var weapons = []
 
 var w_instances = []
+var w_current: int
 
 func _ready():
 	show()
 	for w in weapons: w_instances.append(w.instance())
+	if w_instances.size() > 0: w_current = 0
 
 func hide():
 	$Sprite.modulate.a = 0
@@ -18,3 +20,6 @@ func show():
 	
 func shoot(id: int):
 	w_instances.primary.shoot()
+
+func get_current_weapon () -> Weapon:
+	return w_current and w_instances[w_current]
