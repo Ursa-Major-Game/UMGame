db430e21c6763f0a25cfe1c3de092c1a24269ca6
Youssef <sahli.youssef@ymail.com>
2022-11-12 13:49:46 +0100

    weapon rate of fire default

--- Weapons/Weapon.gd
+++ Weapons/Weapon.gd
@@ -2,5 +2,6 @@ extends Node2D
 class_name Weapon
 
 export (PackedScene) var AmmoType
-export (float, 0.1, 2) var AmmoScale = 1
-export (float) var AmmoSpeed = 100
+export (float, 0.1, 2) var AmmoScale = 1.0
+export (float) var AmmoSpeed = 100.0
+export (int, 1, 50) var RateOfFire = 10 # per seconds
