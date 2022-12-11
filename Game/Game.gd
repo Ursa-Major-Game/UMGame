835ae86554f831fea30b9d066b7805442f2232d9
Youssef <sahli.youssef@ymail.com>
2022-12-11 18:35:01 +0100

    UI changes

--- Game/Game.gd
+++ Game/Game.gd
@@ -4,14 +4,13 @@ export (PackedScene) var PlayerShip
 export (bool) var dbg_straight_to_game = false
 
 var PlayerShipInstance
-var SpawnPoint: Position2D
+onready var SpawnPoint: Position2D = $SpawnPosition2D
 
 func start_game(level: int):
 	SpawnPoint.add_child(PlayerShipInstance)
 	$AsteroidsFactory/Timer.start()
 
 func _ready():
-	SpawnPoint = $SpawnPosition2D
 	PlayerShipInstance = PlayerShip.instance()
 	PlayerShipInstance.get_node("Sprite").modulate.a = 0
 	yield($UILayer, "ui_displayed")
