2566db295b26adec0f49ec5415bd06dd158b4d5d
Youssef <sahli.youssef@ymail.com>
2023-01-01 14:33:30 +0100

    stages advancing

--- Common/Classes/StageNodesHandler.gd
+++ Common/Classes/StageNodesHandler.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal Progressed(seconds)
+signal EndOfStory
 
 export (int, 0, 100) var Progress = 0
 export (String, FILE, "*.story") var StoryFileName
@@ -8,6 +9,24 @@ export (String, FILE, "*.story") var StoryFileName
 var ProgressFloat : float = 0.0
 var StoryConfigFile : ConfigFile
 var LoadedStages := {}
+
+var story_name : String
+var current_stage_id: int = 0 setget set_current_stage_index
+var current_stage : String
+var StageOrder : Array
+
+func set_current_stage_index(idx: int):
+	if idx >= StageOrder.size():
+		ErrorHandler.die("Story " + story_name, "stage index not in stages array")
+	current_stage_id = idx
+	current_stage = StageOrder[idx]
+
+func goto_next_stage():
+	if current_stage_id >= StageOrder.size():
+		emit_signal("EndOfStory")
+		ErrorHandler.die("End of Story" + story_name, "")
+	else:
+		set_current_stage_index(current_stage_id + 1)
 
 func load_level(levelname: String) -> Level:
 	var L : Level = Level.new()
@@ -24,10 +43,14 @@ func load_levels(levelnamedict: Dictionary) -> Dictionary:
 
 func load_stages():
 	for s in StoryConfigFile.get_sections():
-		LoadedStages[s] = load_levels(StoryConfigFile.get_value(s, "levels"))
-		
+		if not (s == "Meta"):
+			LoadedStages[s] = load_levels(StoryConfigFile.get_value(s, "levels"))
+		else:
+			StageOrder = StoryConfigFile.get_value(s, "order")
+
 func start():
 	set_physics_process(true)
+	set_current_stage_index(0)
 
 func _ready():
 	ProgressFloat = float(Progress)
@@ -42,7 +65,7 @@ func _physics_process(delta):
 	if oldProgress != Progress: 
 		emit_signal("Progressed", Progress)
 		if LoadedStages["Stage01"].has(int(Progress)): 
-			LoadedStages["Stage01"][int(Progress)].transfer_nodes_to(get_parent())
+			LoadedStages[current_stage][int(Progress)].transfer_nodes_to(self)
 
 func load_story(s: String) -> ConfigFile:
 	var to = ConfigFile.new()
@@ -50,4 +73,5 @@ func load_story(s: String) -> ConfigFile:
 	var err = to.load(s)
 	if err != OK:
 		ErrorHandler.die("Story not found", "can't open story file: " + s)
+	story_name = s
 	return to
--- Levels/Stories/Entry.story
+++ Levels/Stories/Entry.story
@@ -1,4 +1,7 @@
+[Meta]
+order = ["Stage01"]
+
 [Stage01]
 start_message = "Introduction Story: Test Stage 01"
 end_message = "End of Stage 01"
-levels = {"0101": 5}
+levels = {"0101": 5, "0102": 10}
