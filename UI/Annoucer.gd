c52d9540037e8115a410352c6a2ccb33d73823dc
Youssef <sahli.youssef@ymail.com>
2023-01-04 15:18:20 +0100

    Anouncer message system: added queue

--- UI/Annoucer.gd
+++ UI/Annoucer.gd
@@ -1,6 +1,22 @@
 extends Control
 
+export(Array, String) var message_queue = []
+
+func say_next(text):
+	message_queue.append(text)
+
 func announce(text):
 	if not $AnimationPlayer.is_playing():
 		$Label.text = text
 		$AnimationPlayer.play("fall")
+	else:
+		say_next(text)
+
+
+func _on_Timer_timeout():
+	if not message_queue.empty():
+		announce(message_queue.pop_front())
+
+
+func _on_AnimationPlayer_animation_finished(_anim_name):
+	$Timer.start()
