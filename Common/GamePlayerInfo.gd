52a2e7415b9215040ae6a91903aeb71ee9601557
Youssef <sahli.youssef@ymail.com>
2023-01-04 16:46:11 +0100

    player health handler

--- Common/GamePlayerInfo.gd
+++ Common/GamePlayerInfo.gd
@@ -1,6 +1,18 @@
 extends Node
 
-var score = 0
+signal set_health_bar(health)
+
+var score: int = 0
+export (int) var initial_lifes = 5
+
+var health : int = 100 setget set_health
+onready var lifes : int = initial_lifes
 
 func reset():
 	score = 0
+	lifes = initial_lifes
+	set_health(100)
+
+func set_health(v: int):
+	health = clamp(v, 0, 100)
+	emit_signal("set_health_bar", health)
