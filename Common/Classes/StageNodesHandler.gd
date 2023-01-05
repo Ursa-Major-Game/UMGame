76b85bf09bfe34e6abd92799d885a4bbf7e6e52c
Youssef <sahli.youssef@ymail.com>
2023-01-05 10:20:38 +0100

    Better stage import structure

--- Common/Classes/StageNodesHandler.gd
+++ Common/Classes/StageNodesHandler.gd
@@ -15,6 +15,7 @@ var story_name : String
 var current_stage_id: int = -1 setget set_current_stage_index
 var current_stage : String
 var StageOrder : Array
+var StageHash: Dictionary
 
 var LastLevelTime : float = 0.0
 onready var InitialProgress = Progress
@@ -34,27 +35,24 @@ func goto_next_stage():
 		set_current_stage_index(current_stage_id + 1)
 		emit_signal("say", current_stage)
 
-func load_level(levelname: String) -> Level:
-	var L : Level = Level.new()
-	L.level_name = levelname
-	L.level_data = load("res://Levels/StageNodes/" + levelname + ".tscn")
-	return L
+func get_loaded_stage_levels(stage_name: String) -> Dictionary:
+	var StageConf := load_conf_file("res://Levels/Stages/" + stage_name + ".stage")
+	var LevelDict = {}
+	for s in StageConf.get_sections():
+		var N = StageConf.get_value(s, "name")
+		var L = Level.new()
+		L.level_name = N
+		L.appeance_time = int(s)
+		L.load_data_from_name()
+		LevelDict[int(s)] = L
+		LastLevelTime = max(LastLevelTime, int(s))
+	return LevelDict
 	
-func load_levels(levelnamedict: Dictionary) -> Dictionary:
-	var dict := {}
-	for l in levelnamedict.keys():
-		var app_time = levelnamedict[l]
-		LastLevelTime = max(LastLevelTime, app_time)
-		dict[app_time] = load_level(l)
-	return dict
-
 func load_stages():
 	for s in StoryConfigFile.get_sections():
-		if not (s == "Meta"):
-			LoadedStages[s] = load_levels(StoryConfigFile.get_value(s, "levels"))
-		elif (s == "Meta"):
-			StageOrder = StoryConfigFile.get_value(s, "order")
-
+		StageOrder.append(s)
+		LoadedStages[s] = get_loaded_stage_levels(s)
+		
 func start():
 	set_physics_process(true)
 	goto_next_stage()
@@ -65,7 +63,7 @@ func restartProgress(i : int = InitialProgress):
 
 func _ready():
 	restartProgress()
-	StoryConfigFile = load_story(StoryFileName)
+	StoryConfigFile = load_conf_file(StoryFileName)
 	load_stages()
 	set_physics_process(false)
 
@@ -83,7 +81,7 @@ func _physics_process(delta):
 			if cc == 0: 
 				goto_next_stage()
 
-func load_story(s: String) -> ConfigFile:
+func load_conf_file(s: String) -> ConfigFile:
 	var to = ConfigFile.new()
 	to.clear()
 	var err = to.load(s)
--- Levels/Level.gd
+++ Levels/Level.gd
@@ -6,11 +6,13 @@ export (String) var level_name = "untitled"
 export (float) var appeance_time = 100.0
 export (PackedScene) var level_data
 
+func load_data_from_name():
+	level_data = load("res://Levels/StageNodes/" + level_name + ".tscn")
+
 func transfer_nodes_to(to: Node):
 	var scene = level_data.instance()
 	print("Loaded " + level_name + " : " + String(scene.get_children().size()) + " entities.")
 	for c in scene.get_children():
-		
 		scene.remove_child(c)
 		to.add_child(c)
 		c.set_owner(to)
